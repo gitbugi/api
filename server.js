07812689139c101e6f0933d7ce1f4295e3984a10
gitbugi <oezdemirbu@web.de>
2022-04-02 19:00:41 +0200

    make changes for upload ob heroku

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(
-    process.env.MONGODB_URI,
+    process.env.MONGODB_URI, {
+        server: {
+            socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
+        },
+        replset: {
+            socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
+        }
+
+    },
     (err) => {
         if (err) return console.log("Error: ", err);
         console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
