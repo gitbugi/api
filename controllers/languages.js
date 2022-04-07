bd8d5c2df02e3385355c419b512b34c319f62f6f
gitbugi <oezdemirbu@web.de>
2022-04-07 03:03:12 +0200

    x

--- controllers/languages.js
+++ controllers/languages.js
@@ -1,9 +1,6 @@
 const Languages = require('../models/languages');
 
 const getAllLanguages = (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
     Languages.find({}, (err, data) => {
         if (err) {
             return res.json({ Error: err })
--- controllers/user.js
+++ controllers/user.js
@@ -48,16 +48,6 @@ const register = async(req, res) => {
 }
 */
 const login = async(req, res) => {
-
-    // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Our login logic starts here
     try {
         // Get user input
