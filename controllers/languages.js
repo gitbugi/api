e977730cbd0981db908bcbcaf2d32eea0a4c0c15
gitbugi <oezdemirbu@web.de>
2022-04-07 02:46:28 +0200

    add access-control-allow-origin for language

--- controllers/languages.js
+++ controllers/languages.js
@@ -1,6 +1,9 @@
 const Languages = require('../models/languages');
 
 const getAllLanguages = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     Languages.find({}, (err, data) => {
         if (err) {
             return res.json({ Error: err })
