d4ee85af00dead62d91214010a091042cc3496f8
gitbugi <oezdemirbu@web.de>
2024-03-25 14:30:33 +0100

    bug: error when trying upload 1.6kB file. Set limit for Files to 50mb

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const Textroute = require("./routes/texts");
 const Vocabelsroute = require("./routes/vocabels")
 const userroute = require("./routes/user");
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cors({ origin: '*' }))
 app.use(express.json());
 app.use(helmet());
